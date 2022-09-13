21cd0463a49c66d4fde00cc105324b3bf6f26a97
nao <nao.uru0419@gmail.com>
2022-09-13 14:08:52 +0900

    gtagの削除

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,8 @@
 import '../styles/globals.css'
 import { HeadSetting } from '../components/pages/Head';
 import { AnimatePresence } from 'framer-motion';
-import { GA_TRACKING_ID, pageview } from '../libs/gtag';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps, router }) {
-  
-  // Gtagの設定
-  const route = useRouter();
-  useEffect(() => {
-    if (!GA_TRACKING_ID) return;
-
-    const handleRouteChange = (url) => {
-      pageview(url);
-    };
-    route.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      route.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [route.events]);
 
   return (
     <>
