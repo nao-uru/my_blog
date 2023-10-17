117d0ce964eb7e653f1e1dc75111fed41bc0b83a
NAO <nao@NAOnoMacBook-Air.local>
2023-10-17 10:37:36 +0900

    Fix

--- pages/About.tsx
+++ pages/About.tsx
@@ -66,35 +66,35 @@ export default function AboutPage() {
     <Stack lineHeight="180%" w={{base:"90%", md:"100%"}} ml="auto" m={{base:"auto"}}  mt={{base:6,md:8}} width="100%">
     <Text fontSize={24} fontWeight="bold" mt={6}>Career</Text>
     <Text mt={0} w={{base:"100%",md:"100%"}} mr="auto">
-        法学部に在学中、就職に失敗し、ヨーロッパや東南アジアをバックパック。
+        法学部に在学中に就職に失敗し、ヨーロッパや東南アジアをバックパック。
         その後フリーターをしていました。
-        海外でも通用する仕事をしたいと思っていたところでWEBの世界に興味を持ちました。
-        約1年間、デザインやWEB制作の勉強したのち小さなデザイン会社にアシスタントとして業務しました。<br />
+        世界でも通用するスキルを身につけたいと思い、WEBの世界に入りました。<br />
+        約1年間、デザインやWEB制作の勉強をし、小さなデザイン会社でアシスタントとして業務しました。<br />
         その後は勉強&フリーランスをの期間経て、2022年から正社員として旅行会社のWEB部署でデザイン業務、
-        副業でアプリの制作会社に業務委託として在籍しています。<br />
+        副業でアプリの制作会社に業務委託として在籍しています。
     </Text>
     </Stack>
 
-    <Stack lineHeight="180%" w={{base:"90%", md:"100%"}} ml="auto" m={{base:"auto"}}  mt={{base:6,md:8}}
+    <Stack lineHeight="180%"  mt={{base:6,md:8}}
       borderTop="1px solid #C6CEDA" width="100%">
     <Text fontSize={24} fontWeight="bold" mt={8}>Work</Text>
-    <Text mt={0} w={{base:"100%",md:"100%"}} mr="auto">
+    <Text>
         会社では主にアプリ・WEBデザインをしていて、副業はアプリデザインがメインです。<br />
         個人としてはデザインのお仕事はもちろん、Wordpressdの構築、小規模なWEBサイトのデザイン〜コーディングまでやっていました。
         近年はデザインのお仕事に注力していて、そのキャリアを歩みたいと考えていますがプログラミングも好きなので趣味で学習しています。
         このサイトもデザインから自分で構築しました!（Next.js×contentful）<br />
-        プログラミングの経験を活かした仕様の設計や、エンジニアさんと連携して効率的な開発を進めることが得意です。
+        プログラミングの経験を活かした仕様の設計や、エンジニアさんと連携して効率的な開発を進めることが得意です。WEBデザインの経験もありますが、仕様設計やデザインの管理が必要となるWEBアプリケーションのデザインを行えることが強みです。
     </Text>
     </Stack>
 
-    <Stack lineHeight="180%" w={{base:"90%", md:"100%"}} ml="auto" m={{base:"auto"}}  mt={{base:6,md:8}}
+    <Stack lineHeight="180%" mt={{base:6,md:8}}
       borderTop="1px solid #C6CEDA">
     <Text fontSize={24} fontWeight="bold" mt={8}>Skills</Text>
     <Text fontSize={20} mt={3}>Skill Sets</Text>
     <Text>
     基本的な使用ツールとスキルです。近年はデザインをメインで行なっているので実務でコードはほとんど書いていません。<br />
-    これらに加えNotion・Backlogを用いたタスク管理、GitHubを使ったコード管理の等経験があります。<br />
-    フルリモートの会社に属した経験もあるのでリモート下でも問題なくコミュニケーションをとることができます。
+    これらに加えNotion・Backlogを用いたタスク管理、GitHubを使ったコード管理などの経験があります。<br />
+    フルリモートの会社に属した経験もあるのでリモート下でも問題なくコミュニケーションをとることができます。伝わりやすく、お互いに気持ちよく仕事ができるコミュニケーションを心がけています。
     </Text>
     <Grid templateColumns={{sm:'repeat(1, 1fr)',md:'repeat(2, 1fr)',lg:'repeat(3, 1fr)'}} rowGap={3} columnGap={3}>
     <SkillCard src='/img/Skills/figma.png' title="figma" text="メインで使用しているツールです。オートレイアウトはもちろん、デザインシステムや管理の仕方も考えながら使用しています。" />
@@ -102,29 +102,30 @@ export default function AboutPage() {
     <SkillCard src='/img/Skills/photoshop.png' title="Photoshop" text="仕事ではほとんど使わないため、練習やスキルアップのために学習中です。" />
     <SkillCard src='/img/Skills/html.png' title="HTML CSS" text="特異に複雑でない限り問題なくコーディングができます。LPやHPのコーディング経験があります。" />
     <SkillCard src='/img/Skills/js.png' title="JavaScript" text="複雑なコードを書くことは難しいですが、たいていの動きは調べながら作成できます。" />
-    <SkillCard src='/img/Skills/react.png' title="React" text="実務経験はありません。完全に独学で勉強をしています。記事や動画を調べて四苦八苦しています。このサイトもReat(Next.js)で構築しました。" />
+    <SkillCard src='/img/Skills/react.png' title="React" text="実務経験はありません。独学なので記事や動画を調べ、四苦八苦しています。このサイトもReat(Next.js)で構築しました。" />
     </Grid>
-    <Flex mt={6} direction={{base:"column",md:"row"}}>
+    <Flex mt={6} direction={{base:"column",md:"row"}} gap={4}>
     <Box w={{base:"100%",md:"50%"}}>
     <Text fontSize={20} mt={3}>Skill Maps</Text>
-    <Text w={{base:"100%",md:"100%"}} mr="auto">
+    <Text>
     アプリのUI/UXデザインが一番得意で好きです。また、コンポーネントの管理方法やデザインのシステム化に興味があり、日頃から情報をキャッチアップしています。
     最近参考にしているのは「デザイン庁 デザインシステム」と「iOS17(Apple Design Resources)」です。<br />
-    アプリケーションのデザインには、状態変化や画面遷移などグラフィックやWEBデザインとは少し違う部分があります。そこに関してプログラミングの経験を活かし、エンジニアさんと協力して取り組むとこができます。<br />
+    アプリケーションのデザインには、状態変化や画面遷移などグラフィックやWEBデザインとは少し違う部分があります。それらの部分に対して経験を活かし、エンジニアさんと協力して取り組むとこができます。<br />
 
     </Text>
     </Box>
-    <Image p={4} className="image" src="/img/Skills/chart.png" w={{base:"100%",md:"50%"}} alt="Skill maps" />
+    <Image className="image" src="/img/Skills/chart.png" w={{base:"100%",md:"50%"}} alt="Skill maps" />
     </Flex>
     </Stack>
 
     <Stack lineHeight="180%" w={{base:"90%", md:"100%"}} ml="auto" m={{base:"auto"}}  mt={{base:6,md:8}}
       borderTop="1px solid #C6CEDA" width="100%">
-    <Text fontSize={24} fontWeight="bold" mt={8}>Goal</Text>
-    <Text w={{base:"100%",md:"100%"}} mr="auto">
+    <Text fontSize={24} fontWeight="bold" mt={8}>Vsion</Text>
+    <Text>
       将来的にはマネジメントや、ディレクションの業務に携わりたいと思っています。しかし、まだまだ手を動かしてデザインに携わりたいという気持ちが強いです。
       最近は新人さんを教えながらリーダーとしてデザイン制作に取り組んでいて、教えることが自分の成長にもつながっていると実感しています。<br />
-      進行係としてのマネジメントではなく、しっかりとデザイナーとしての実績を積み、経験を活かしたマネージャーやリーダーがになることが理想です。
+      進行係としてのマネジメントではなく、しっかりとデザイナーとしての実績を積み、経験を活かしたマネージャーになることが理想です。<br />
+      また、ユーザーに対する理解が足りないと感じることが多いのでユーザー心理を理解するための要件定義方法やその意義についての知識をもっと増やしたいです。そのため今は心理学や行動経済学の本を読んで勉強中です！
     </Text>
     </Stack>
 
