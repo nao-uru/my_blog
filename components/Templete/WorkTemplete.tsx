7bb4a91f4ddba959d77c096c7cfa6a187e3dec38
NAO <nao@NAOnoMacBook-Air.local>
2024-02-14 16:28:07 +0900

    Add OOUI Part2

--- components/Templete/WorkTemplete.tsx
+++ components/Templete/WorkTemplete.tsx
@@ -23,7 +23,7 @@ export const Work = () => {
 
       {/* IDEAL ME */}
       <Box mt={4}  maxW={{base:"100%", md:"1000px"}}  m="auto" p={{base:4, md:6}} backgroundColor="#ffffff" rounded="2xl">
-      <Text fontWeight='bold' fontSize={{base:"24px",md:"32px"}}>IDEAL ME</Text>
+      <Text fontWeight='bold' fontSize={{base:"20px",md:"28px"}}>IDEAL ME</Text>
       <Text fontWeight='nomal' fontSize="md" mb={4} >オリジナルのアプリをデザインしました！要件定義〜デザインまですべて一人で制作しました。デザインのフローは下記の記事に記しました。また、figmaのファイルとPDFを公開しています。<br />
       昨日に焦点を当てたセルフケアアプリです。シンプルな構造で男女ともに使いやすいアプリを目指しました。
       </Text>
@@ -31,10 +31,8 @@ export const Work = () => {
       <Flex direction={{base:"column", md:"row"}} justifyContent="space-between" alignItems="flex-start" mt={2} gap={{base:4, md:4}}>
       <Box w={{base:"full", md:"60%"}} mr={0}>
       <iframe src="https://drive.google.com/file/d/192FfFPlX5wJIA3skCsDC3O2FGBgvc9LS/preview" width="100%" height="320px"></iframe>
-      {/* <Image src={"/img/ideallMe.png"} rounded="xl" alt="Work thubmnail" w="400px"  fit='cover' /> */}
       <Flex direction={{base:'column', md:"row"}} my={{base:4, md:6}} gap={{base:3, md:0}}>
-        <ButtonPrime href="https://www.figma.com/file/2r3Lm085yw0U6E9iVLi7Ta/IDEAL-ME?type=design&node-id=503%3A25036&mode=design&t=XTGedAxHjyVln4np-1"wide={{base:"full", md:"fit-content"}} bgColor="#25D088" target="_blank" color="#ffffff" icon={''}>figmaファイルはこちら</ButtonPrime>
-        <ButtonPrime href="https://drive.google.com/file/d/1bsB8Z7Z9nRi8DopsTeS-0xSBzJne47Du/view?usp=sharing" bgColor="#ffffff" border="#25D088" target="_blank" color="#25D088"  wide={{base:"full", md:"fit-content"}} icon={''}>PDF(ファイルの書き出し)</ButtonPrime>
+      <ButtonPrime href="https://www.figma.com/file/2r3Lm085yw0U6E9iVLi7Ta/IDEAL-ME?type=design&node-id=503%3A25036&mode=design&t=XTGedAxHjyVln4np-1"wide={{base:"full", md:"fit-content"}} bgColor="#25D088" target="_blank" color="#ffffff" icon={''}>figmaファイルはこちら</ButtonPrime>
       </Flex>
       </Box>
       <Box w={{base:"full", md:"40%"}}> 
@@ -54,9 +52,9 @@ export const Work = () => {
 
        {/* OOUI1 */}
       <Box  maxW={{base:"100%", md:"1000px"}} m="auto" p={6} backgroundColor="#ffffff" rounded="2xl" mt={{base:4, md:12}}>
-      <Text fontWeight='bold' fontSize={{base:"24px",md:"32px"}}>OOUI実践 Part1</Text>
-      <Text fontWeight='nomal' fontSize="md" mb={4} >オブジェクト指向デザインの「オブジェクト指向UIデザイン」の応用編の課題を拝借してデザインを作成しています。<br />
-      こちらは応用編レベル10の「スマートフォン用 営業支援アプリ」です。本にある課題はもちろん、自分でも設定を追加して、より便利なアプリを目指しました。目指せシリーズ化！</Text>
+      <Text fontWeight='bold' fontSize={{base:"20px",md:"28px"}}>OOUI実践1 「営業支援アプリ」</Text>
+      <Text fontWeight='nomal' fontSize="md" mb={4} >「オブジェクト指向UIデザイン」の課題を拝借してデザインを作成しました。<br />
+      こちらは応用編レベル10の「営業支援アプリ」です。本にある課題はもちろん、自分でも設定を追加して、より便利なアプリを目指しました。目指せシリーズ化！</Text>
       <Flex direction={{base:"column", md:"row"}} justifyContent="space-between" alignItems="flex-start" mt={2} gap={{base:4, md:4}}>
       <Box w={{base:"full", md:"60%"}} mr={0}>
       <iframe src="https://drive.google.com/file/d/19uRqJW35kGEZwuy_8I1NL70Wdf5fD2YJ/preview" width="100%" height="320px"></iframe>
@@ -76,10 +74,34 @@ export const Work = () => {
       </Flex>
       </Box>
 
+      {/* OOUI2 */}
+      <Box maxW={{base:"100%", md:"1000px"}} m="auto" p={6} backgroundColor="#ffffff" rounded="2xl" mt={{base:4, md:12}}>
+      <Text fontWeight='bold' fontSize={{base:"20px",md:"28px"}}>OOUI実践2 「イベント管理アプリ」</Text>
+      <Text fontWeight='nomal' fontSize="md" mb={4} >「オブジェクト指向UIデザイン」の応用編の課題を拝借してデザインを作成しました。<br />
+      第2弾となるこちらは応用編レベル11の「イベント管理アプリ」です。</Text>
+      <Flex direction={{base:"column", md:"row"}} justifyContent="space-between" alignItems="flex-start" mt={2} gap={{base:4, md:4}}>
+      <Box w={{base:"full", md:"60%"}} mr={0}>
+      <iframe src="https://drive.google.com/file/d/1BO9jD0ce-QKqRMERAnD98dOLW-VLjJIq/preview" width="100%" height="320px"></iframe>
+
+      <Flex direction={{base:'column', md:"row"}} my={{base:4, md:6}} gap={{base:3, md:0}}>
+        <ButtonPrime href="https://www.figma.com/file/6rmHOT3FTgVR0v28LkECqS/%E6%A5%AD%E5%8B%99%E3%82%A2%E3%83%97%E3%83%AA?type=design&node-id=1790-27203&mode=design&t=VZwR4WJqpOZ6gXX6-11" wide={{base:"full", md:"fit-content"}} bgColor="#25D088" target="_blank" color="#ffffff" icon={''}>figmaファイルはこちら</ButtonPrime>
+      </Flex>
+      </Box>
+      <Box w={{base:"full", md:"40%"}}> 
+        <Box>
+        <BlogCardSmall
+        title="OOUI実践してみた 「イベント管理アプリ」" 
+        url="//images.ctfassets.net/dd0roywjqx3t/6RMcxuThsQncawb17EkqUB/23d862cf0cbb221f51cb40b70a6d079b/Thubmneil_Blog.png" 
+        slug="https://www.naosjournal.com/ooiu_2" />
+        </Box>
+      </Box>
+      </Flex>
+      </Box>
+
       {/* 制作実績 */}
       <Box maxW={{base:"100%", md:"1000px"}} m="auto" p={6} backgroundColor="#ffffff" rounded="2xl" mt={{base:4, md:12}}>
       <Text fontWeight='bold' fontSize={{base:"24px",md:"32px"}}>制作実績</Text>
-      <Text fontWeight='nomal' fontSize="md" mb={4} >制作実績は非公開となっております。ご興味をお持ちの方はお問い合わせください！採用担当者様には別途お送りしております。</Text>
+      <Text fontWeight='nomal' fontSize="md" mb={4} >業務で担当した実績は非公開となっております。ご興味をお持ちの方はお問い合わせください！<br />採用担当者様には別途お送りしております。</Text>
 
       <Image src={"/img/portfolio.png"} rounded="xl" alt="Work thubmnail" w="400px"  fit='cover' mt={2} />
       <Flex direction={{base:'column', md:"row"}} my={{base:4, md:6}} gap={{base:2, md:0}}>
@@ -89,8 +111,6 @@ export const Work = () => {
      
 
       </Box>
-
-
        {/* Dribble */}
       <Box>
       <Box mt={16}>
